32fd5783347664b8cbca84c66c6e30b25e79ba23
Fabio Coimbra <fabiogc1989@gmail.com>
2023-03-25 20:44:10 +0000

    add reset button

--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({super.key});
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  const Result(this.resultScore, this.resetHandler, {super.key});
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are .... strange?!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('You did it!'),
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: const TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: resetHandler,
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.blue)),
+            child: const Text('Restart Quiz!'),
+          ),
+        ],
+      ),
     );
   }
 }
